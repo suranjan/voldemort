cfa05e77be385f87e44e1e719d86c7972107385d
suranjan <suranjan.kumar@gmail.com>
2010-09-19 15:04:46 +0530

    -- First change to check git.

--- src/java/voldemort/VoldemortAdminTool.java
+++ src/java/voldemort/VoldemortAdminTool.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2008-2010 LinkedIn, Inc
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -64,6 +64,9 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
+/**
+ * surnajan is modifying this file.
+ */
 /**
  * Provides a command line interface to the
  * {@link voldemort.client.protocol.admin.AdminClient}
